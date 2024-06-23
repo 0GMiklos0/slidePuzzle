cf7cad7bd1932cc981e223f161a4e71904674e46
0GMiklos0 <90982702+0GMiklos0@users.noreply.github.com>
2024-06-23 10:46:47 +0200

    Add files via upload

--- src/main/java/model/Moves.java
+++ src/main/java/model/Moves.java
@@ -1,8 +1,17 @@
 package model;
 
-import model.Cards;
-import model.Direction;
-
 public record Moves(Cards card, Direction direction) {
+    public int getNumber(){
+        return card().number();
+    }
 
+    public Moves nextMove(){
+        return new Moves(card().move(this.direction()), this.direction());
+    }
+
+    public Cards nextMoveCard() {
+        return new Cards(this.card().number(),
+                this.card().move(this.direction).row(),
+                this.card().move(this.direction).col());
+    }
 }
--- src/main/java/model/slidingPuzzle.java
+++ src/main/java/model/slidingPuzzle.java
@@ -1,16 +1,38 @@
 package model;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-public class slidingPuzzle {
+public class slidingPuzzle implements State<Moves> {
     private Cards[] cards;
 
-    private Tile[][] tiles;
+    private int[][] tiles;
+    public slidingPuzzle(){
+        cards = new Cards[]{
+                new Cards(1,1,9),
+                new Cards(2,1,1),
+                new Cards(3,1,2),
+                new Cards(4,1,3),
+                new Cards(5,1,4),
+                new Cards(6,1,5),
+                new Cards(7,1,6),
+                new Cards(8,1,7),
+                new Cards(9,1,8),
+        };
+        tiles = new int[][]{
+                {-1,-1,-1,0,-1,0,-1,0,-1}, {0,0,0,0,0,0,0,0}
+        };
+    }
 
+    public Cards[] cardOrder(){
+        return (Cards[]) Arrays.stream(cards).sorted(Comparator.comparingInt(c -> c.row() + c.col())).toArray();
+    }
+
+    public void makeMove(Moves move){
+        cards[move.getNumber()-1] = move.nextMoveCard();
+    }
     public boolean isLegalMove(Moves move){
-        int[] theMove = {move.card().move(move.direction()).row(),  move.card().move(move.direction()).col()};
-        return tiles[theMove[0]][theMove[1]].equals(Tile.REACHABLE);
+        int[] theMove = {move.nextMove().card().row(), move.nextMove().card().col()};
+        return tiles[theMove[0]][theMove[1]]==0;
     }
 
     public Set<Moves> getLegalMoves(){
@@ -22,5 +44,12 @@ public class slidingPuzzle {
             }
         }
         return legalMoves;
+    }
+
+    public boolean isSolved(){
+        for (int i = 0; i < cardOrder().length; i++){
+            if(!(cardOrder()[i].number() == i+1)) return false;
+        }
+        return true;
     }
 }
